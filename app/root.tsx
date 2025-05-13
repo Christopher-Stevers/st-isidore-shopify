112f77f5250354a6d86396967be96f342b9d3825
Christopher-Stevers <christopher.stevers@protonmail.com>
2025-05-13 10:11:22 -0400

    deploy with new pixel

--- app/root.tsx
+++ app/root.tsx
@@ -181,20 +181,19 @@ function Layout({children}: {children?: React.ReactNode}) {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta name="msvalidate.01" content="A352E6A0AF9A652267361BBB572B8468" />
 
+        {/* Meta Pixel Code */}
         <script
           dangerouslySetInnerHTML={{
-            __html: `
-      !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '1037492821816183');
-      fbq('track', 'PageView');
-    `,
+            __html: `!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '718040284049560');
+            fbq('track', 'PageView');`,
           }}
         />
         <noscript>
@@ -202,10 +201,11 @@ function Layout({children}: {children?: React.ReactNode}) {
             height="1"
             width="1"
             style={{display: 'none'}}
-            src="https://www.facebook.com/tr?id=1037492821816183&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=718040284049560&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
+        {/* End Meta Pixel Code */}
         <Meta />
         <Links />
       </head>
