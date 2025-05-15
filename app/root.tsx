78803e3ecb3c0414fe61a4c47ae300a127dffb71
Christopher-Stevers <christopher.stevers@protonmail.com>
2025-05-15 09:31:51 -0400

    update pixel

--- app/root.tsx
+++ app/root.tsx
@@ -184,27 +184,24 @@ function Layout({children}: {children?: React.ReactNode}) {
         {/* Meta Pixel Code */}
         <script
           dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '718040284049560');
-            fbq('track', 'PageView');`,
+            __html: ` !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1235261444704639');
+        fbq('track', 'PageView');`,
           }}
         />
+
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `<img
-            height="1"
-            width="1"
-            style={{display: 'none'}}
-            src="https://www.facebook.com/tr?id=718040284049560&ev=PageView&noscript=1"
-            alt=""
-          />`,
+            __html: `<img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=1235261444704639&ev=PageView&noscript=1"
+        />`,
           }}
         />
         {/* End Meta Pixel Code */}
